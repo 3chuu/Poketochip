f8fd30b8006ffa670d86910c3081fc9cd71f34cc
cha_nyeong <empty005@daum.net>
2023-11-13 18:04:24 +0900

    Feat: Tuist 초기 설정으로 변경

---
+++ Poketochip/Targets/PoketochipKit/Tests/PoketochipKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class PoketochipKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("PoketochipKit", "PoketochipKit")
+    }
+}
---
+++ Poketochip/Targets/PoketochipUI/Tests/PoketochipUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class PoketochipUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("PoketochipUI", "PoketochipUI")
+    }
+}
