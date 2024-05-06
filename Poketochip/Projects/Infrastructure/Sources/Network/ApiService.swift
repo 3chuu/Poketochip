2e2b2844eae34b9db69f8f890511fdfa2fa99c54
cha_nyeong <empty005@daum.net>
2024-05-07 00:16:47 +0900

    Feat : Network 수정 진행

---
+++ Poketochip/Projects/Infrastructure/Sources/Network/ApiService.swift
@@ -0,0 +1,16 @@
+//
+//  ApiService.swift
+//  Infrastructure
+//
+//  Created by cha_nyeong on 5/6/24.
+//
+
+import Foundation
+import Alamofire
+import RxSwift
+
+public class ApiService {
+    func request() {
+        
+    }
+}
---
+++ Poketochip/Projects/Infrastructure/Sources/Network/Config.swift
@@ -0,0 +1,71 @@
+//
+//  Config.swift
+//  Infrastructure
+//
+//  Created by cha_nyeong on 5/3/24.
+//
+
+import Foundation
+
+protocol BaseUrlProtocol {
+    var baseApiUrl: String { get }
+}
+
+enum Configs {
+    enum App {
+        // 둘 중에 마음 드는 네이밍으로 가죵!
+        static let baseApiUrl = "https://pokeapi.co/api/v2"
+        static let pokeApiUrl = "https://pokeapi.co/api/v2"
+    }
+
+    enum Endpoint: String {
+        case ability = "ability/"
+        case berry = "berry/"
+        case berryFirmness = "berry-firmness/"
+        case berryFlavor = "berry-flavor/"
+        case characteristic = "characteristic/"
+        case contestEffect = "contest-effect/"
+        case contestType = "contest-type/"
+        case eggGroup = "egg-group/"
+        case encounterCondition = "encounter-condition/"
+        case encounterConditionValue = "encounter-condition-value/"
+        case encounterMethod = "encounter-method/"
+        case evolutionChain = "evolution-chain/"
+        case evolutionTrigger = "evolution-trigger/"
+        case gender = "gender/"
+        case generation = "generation/"
+        case growthRate = "growth-rate/"
+        case item = "item/"
+        case itemAttribute = "item-attribute/"
+        case itemCategory = "item-category/"
+        case itemFlingEffect = "item-fling-effect/"
+        case itemPocket = "item-pocket/"
+        case language = "language/"
+        case location = "location/"
+        case locationArea = "location-area/"
+        case machine = "machine/"
+        case move = "move/"
+        case moveAilment = "move-ailment/"
+        case moveBattleStyle = "move-battle-style/"
+        case moveCategory = "move-category/"
+        case moveDamageClass = "move-damage-class/"
+        case moveLearnMethod = "move-learn-method/"
+        case moveTarget = "move-target/"
+        case nature = "nature/"
+        case palParkArea = "pal-park-area/"
+        case pokeathlonStat = "pokeathlon-stat/"
+        case pokedex = "pokedex/"
+        case pokemon = "pokemon/"
+        case pokemonColor = "pokemon-color/"
+        case pokemonForm = "pokemon-form/"
+        case pokemonHabitat = "pokemon-habitat/"
+        case pokemonShape = "pokemon-shape/"
+        case pokemonSpecies = "pokemon-species/"
+        case region = "region/"
+        case stat = "stat/"
+        case superContestEffect = "super-contest-effect/"
+        case type = "type/"
+        case version = "version/"
+        case versionGroup = "version-group/"
+    }
+}
